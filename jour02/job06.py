d29010873b64bfa79bf953cec8b9480bdf232d40
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-07 19:13:33 +0100

    Create job06.py

---
+++ jour02/job06.py
@@ -0,0 +1,13 @@
+def positif_ou_negatif(number):
+
+    if number < 0:
+        print("negatif")
+    elif number > 0:
+        print("positif")
+    else :
+      print("You have zero in your numbres great job")
+
+positif_ou_negatif(-2)
+positif_ou_negatif(5)
+positif_ou_negatif(-6)
+positif_ou_negatif(0)
